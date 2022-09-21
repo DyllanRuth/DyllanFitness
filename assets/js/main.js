54dd991eb64949eef5996f0967efdd8a4d294b1f
Dyllan.ruth@gmail.com <you@example.com>
2022-09-20 22:10:44 -0500

    Accordion Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,90 +86,33 @@ sr.reveal(`.choose__img, .calculate__content`, {origin: 'left'})
 sr.reveal(`.choose__content, .calculate__img`, {origin: 'right'})
 
 /*=============== ACCORDION JS  ===============*/
-const accordionItems = document.querySelectorAll('.accordion__items')
+const accordionItems = document.querySelectorAll('.accordion__item')
 
 accordionItems.forEach((item) =>{
     const accordionHeader = item.querySelector('.accordion__header')
 
     accordionHeader.addEventListener('click', () =>{
         const openItem = document.querySelector('.accordion-open')
-
+        console.log('click')
         toggleItem(item)
 
         if(openItem && openItem!== item){
             toggleItem(openItem)
         }
-
     })
-
-
 })
 
 const toggleItem = (item) =>{
     const accordionContent = item.querySelector('.accordion__content')
-
     if(item.classList.contains('accordion-open')){
         accordionContent.removeAttribute('style')
         item.classList.remove('accordion-open')
     } else {
         accordionContent.style.height = accordionContent.scrollHeight + 'px'
         item.classList.add('accordion-open')
-    }   
-}
-
-/*=============== CALCULATE JS ===============*/
-const calculateForm = document.getElementById('calculate-form'),
-      calculateCm = document.getElementById('calculate-cm'),
-      calculateKg = document.getElementById('calculate-kg'),
-      calculateMessage = document.getElementById('calculate-message')
-
-const calculateBmi = (e) =>{
-    e.preventDefault()
-    
-    // Check if the fields have a value
-    if(calculateCm.value === '' || calculateKg.value === ''){
-        // Add and remove color
-        calculateMessage.classList.remove('color-green')
-        calculateMessage.classList.add('color-red')
-
-        // Show message
-        calculateMessage.textContent = 'Fill in the Height and Weight 👨‍💻'
-
-        // Remove message three seconds
-        setTimeout(() => {
-            calculateMessage.textContent = ''
-        }, 3000);
-    }else{
-        // BMI Formula
-        const cm = calculateCm.value / 100,
-              kg = calculateKg.value,
-              bmi = Math.round(kg / (cm * cm))
-
-        // Show your health status
-        if(bmi < 18.5){
-            // Add color and display message
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😔`
-        } else if(bmi < 25){
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy 🥳`
-        }else{
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight 😔`
-        }
-
-        // To clear the input field
-        calculateCm.value = ''
-        calculateKg.value = ''
-
-        // Remove message four seconds
-        setTimeout(() => {
-            calculateMessage.textContent = ''
-        }, 4000);
     }
 }
 
-calculateForm.addEventListener('submit', calculateBmi)
 
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('contact-form'),
