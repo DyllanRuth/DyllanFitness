4adb44029331cef713b5626ed573d85f0c93eea1
Dyllan.ruth@gmail.com <you@example.com>
2022-09-21 23:38:41 -0500

    Addinf Scheduling and functional changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,12 +75,12 @@ window.addEventListener('scroll', scrollUp)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 1500,
+    delay: 10,
 })
 
 sr.reveal(`.home__data, .footer__container, .footer__group`)
-sr.reveal(`.home__img`, {delay: 700, origin: 'bottom'})
+sr.reveal(`.home__img`, {delay: 120, origin: 'bottom'})
 sr.reveal(`.logos__img, .program__card, .pricing__card`, {interval: 100})
 sr.reveal(`.choose__img, .calculate__content`, {origin: 'left'})
 sr.reveal(`.choose__content, .calculate__img`, {origin: 'right'})
